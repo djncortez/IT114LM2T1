883ca28887a956ec2b5b96dfae44976a56b36a63
djncortez <155616798+djncortez@users.noreply.github.com>
2024-02-28 22:20:42 +0800

    Update Exercise1.aspx.cs

--- Module2Topic1/Exercise1.aspx.cs
+++ Module2Topic1/Exercise1.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +10,19 @@ namespace Module1Exercise1
     public partial class Exercise1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string fsong = "My Funny Valentine - Frank Sinatra";
+            song.InnerText = "Now playing " + fsong;
+        }
+        protected void Button3_Click(object sender, EventArgs e)
         {
 
         }
